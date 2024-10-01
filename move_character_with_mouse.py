434afebd911521ea7f180aff1b85c211f91e4af9
minwooji <jimw0201@gmail.com>
2024-10-01 20:31:04 +0900

    캐릭터 좌표 계산 / 캐릭터가 손화살표를 따라가게 함

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -22,6 +22,8 @@ running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 hand_x, hand_y = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)    # 손화살표의 좌표 x, y(0부터 배경 너비와 높이 사이 랜덤한 값)
 frame = 0
+t = 0.0       # t = 0.0 : 캐릭터 시작 위치, t = 1.0 : 손화살표 위치 도달, 0 <= t <= 1
+speed = 0.01  # 캐릭터 이동 속도
 hide_cursor()
 
 while running:
@@ -31,6 +33,12 @@ while running:
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
+
+    # 식 m(t) = (1 - t)*p1 + t * p2 로 캐릭터의 좌표를 계산 / p1 : 캐릭터의 위치, p2 : 손화살표의 위치
+    x = (1 - t) * x + t * hand_x
+    y = (1 - t) * y + t * hand_y
+
+    t += speed      # t 값을 speed(이동 속도)만큼 점진적으로 증가
 
     handle_events()
 
