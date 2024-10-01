ca85cc3468daa4a66c1d8c9602b08ff6c4c3660e
minwooji <jimw0201@gmail.com>
2024-10-01 16:45:52 +0900

    필요한 이미지 로드 / 배경과 캐릭터를 그림

---
+++ move_character_with_mouse.py
@@ -0,0 +1,36 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+hand_arrow = load_image('hand_arrow.png')
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    pass
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+hide_cursor()
+
+while running:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+
+    handle_events()
+
+    delay(0.05)
+
+close_canvas()
